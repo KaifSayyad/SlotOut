1d699be8b85804ef1f0db23f417af4a060e3f0b5
KaifAliSayyad <Kaifali.Sayyad@wissen.com>
2025-06-10 02:02:32 +0530

    sync

--- v1/src/main/java/com/slotout/v1/configurations/SecurityConfig.java
+++ v1/src/main/java/com/slotout/v1/configurations/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.Arrays;
 
 @Configuration
